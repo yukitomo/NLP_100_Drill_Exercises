f45bf06cc66787a1f11a20dd0eda715232721ef7
yukitomo <over.the.tr0ouble@gmail.com>
2014-10-16 15:37:39 +0900

    test1.py add

--- test1.py
+++ test1.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python
 #-*-coding:utf-8-*-
-import sys
+#(1) 行数をカウントしたもの．確認にはwcコマンドを用いよ．
+#確認 bash test1.sh
+
 a = 0
-f1=open('col1.txt','w')
-f2=open('col2.txt','w')
+f=open('address.txt','r')
 
-for line in open(sys.argv[1]):
-	a+=1
-	line2=line.replace("	"," ")
-	print line2,
-	itemList = line2[:-1].split(' ')
-	#print itemList[1]
-	f1.write(itemList[0]+"\n")
-	f2.write(itemList[1]+"\n")
-	
+for line in f:
+	a += 1
+
 print a
-#print sys.argv[0]
-#print sys.argv[1]
-
-
-f1.close()
-f2.close()
-
-
